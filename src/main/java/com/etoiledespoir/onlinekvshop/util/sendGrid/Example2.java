bb677ebc75be611e982190809995a2d079b6f538
ESPOIR DITEE <espoirditekemena@gmail.com>
2019-11-28 20:54:37 +0200

    Update Example2.java

--- src/main/java/com/etoiledespoir/onlinekvshop/util/sendGrid/Example2.java
+++ src/main/java/com/etoiledespoir/onlinekvshop/util/sendGrid/Example2.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 
 public class Example2 {
     public static void main(String[] args) throws IOException {
-        Email from = new Email("216093805@mycput.ac.za");
         String subject = "Sending with Twilio SendGrid is Fun";
         Email to = new Email("espoirditekemena@gmail.com");
         Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
         Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(System.getenv("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"));
         Request request = new Request();
         try {
             request.setMethod(Method.POST);
