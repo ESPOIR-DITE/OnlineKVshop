da402ca5b671807cbe69055d7f40936305124abe
ESPOIR DITEE <espoirditekemena@gmail.com>
2019-11-28 19:41:23 +0200

    Update SendEmailSMTP.java

--- src/main/java/com/etoiledespoir/onlinekvshop/util/email/SendEmailSMTP.java
+++ src/main/java/com/etoiledespoir/onlinekvshop/util/email/SendEmailSMTP.java
@@ -14,9 +14,8 @@ public class SendEmailSMTP {
 
     private static final String SMTP_SERVER = "smtp.sendgrid.net";
     private static final String USERNAME = "apikey";
-    //private static final String PASSWORD = "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";
+    
 
-    private static final String PASSWORD = "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";
     private static final String EMAIL_FROM = "216093805@mycput.ac.za";
 
     private static final String EMAIL_TO_CC = "";
